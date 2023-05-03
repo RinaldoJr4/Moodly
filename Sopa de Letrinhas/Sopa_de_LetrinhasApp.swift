21ac9284d0f3c3794b9f3383e560b8260b86a03e
Rinaldo Jr <107890806+RinaldoJr4@users.noreply.github.com>
2023-05-03 13:29:16 -0300

    initial commit

---
+++ Sopa de Letrinhas/Sopa_de_LetrinhasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sopa_de_LetrinhasApp.swift
+//  Sopa de Letrinhas
+//
+//  Created by rsbj on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Sopa_de_LetrinhasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
