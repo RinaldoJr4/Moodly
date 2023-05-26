955cc0fa987ddefe248de381c01149fc010ad8de
Rinaldo Jr <107890806+RinaldoJr4@users.noreply.github.com>
2023-05-26 12:01:26 -0300

    refining MainView

--- Sopa de Letrinhas/BoardView.swift
+++ Sopa de Letrinhas/BoardView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct BoardView: View {
+    
+    var currentDate = Date()
+    var currentDateCalendar = Calendar.current
+    var dateFormater: DateFormatter {
+        let formater = DateFormatter()
+        formater.dateStyle = .medium
+        return formater
+    }
+    
     var body: some View {
         GeometryReader { geo in
             HStack {
+                
                 VStack {
                     HStack {
                         Text("Bom dia, vamos planejar suas atividades?")
@@ -25,6 +35,7 @@ struct BoardView: View {
                     Rectangle().foregroundColor(.gray)
                     
                 }.padding()
+                
                 VStack {
                     HStack{
                         Spacer()
@@ -32,27 +43,45 @@ struct BoardView: View {
                             .frame(width: 2)
                             .layoutPriority(101)
                         VStack{
-                            Text("24")
+                            Text("\(currentDateCalendar.component(.day, from: currentDate))")
                                 .font(.system(size: 80))
                                 .bold()
                                 .foregroundColor(.black)
                                 .minimumScaleFactor(0.8)
-                            Text("Mai")
+                            Text(String(dateFormater.string(from: currentDate)).dropLast(5).dropFirst(3))
                                 .font(.largeTitle)
                                 .foregroundColor(.black)
                                 .minimumScaleFactor(0.8)
                         }.padding(.trailing, geo.size.width/30)
                     }
+                    
                     Spacer().layoutPriority(100)
                     ZStack{
                         VStack{
                             HStack{
-                                // Postit Azul
-                                Rectangle().foregroundColor(.blue)
+                                // Sombra Postit Azul
+                                Rectangle().foregroundColor(Color(white: 0.7))
+                                        .frame(width: geo.size.width/5, height: geo.size.width/5)
+                                Spacer()
+                            }.padding(.top,geo.size.width/23)
+                                .padding(.leading,geo.size.width/9.70)
+                            HStack{
+                                Spacer()
+                                Rectangle().foregroundColor(.clear)
                                     .frame(width: geo.size.width/5, height: geo.size.width/5)
-                                    .padding(.leading,geo.size.width/10)
+                            }
+                        }
+                        VStack{
+                            HStack{
+                                // Postit Azul
+                                ZStack{
+                                    Rectangle().foregroundColor(.blue)
+                                        .frame(width: geo.size.width/5, height: geo.size.width/5)
+                                    Text("Postit azul")
+                                }
                                 Spacer()
                             }.padding(.top,geo.size.width/25)
+                                .padding(.leading,geo.size.width/10)
                             HStack{
                                 Spacer()
                                 Rectangle().foregroundColor(.clear)
@@ -65,11 +94,27 @@ struct BoardView: View {
                                     .frame(width: geo.size.width/5, height: geo.size.width/5)
                             }
                             HStack{
+                                // Sombra Postit Vermelho
+                                Spacer()
+                                    Rectangle().foregroundColor(Color(white: 0.7))
+                                        .frame(width: geo.size.width/5, height: geo.size.width/5)
+                            }.padding(.bottom,geo.size.width/30)
+                        }
+                        VStack{
+                            HStack{
+                                Spacer()
+                                    .frame(width: geo.size.width/5, height: geo.size.width/5)
+                            }
+                            HStack{
                                 // Postit Vermelho
                                 Spacer()
-                                Rectangle().foregroundColor(.red)
-                                    .frame(width: geo.size.width/5, height: geo.size.width/5)
+                                ZStack {
+                                    Rectangle().foregroundColor(.red)
+                                        .frame(width: geo.size.width/5, height: geo.size.width/5)
+                                    Text("Reflex vermelho")
+                                }
                             }.padding(.bottom,geo.size.width/25)
+                                .padding(.trailing,geo.size.width/300)
                         }
                     }.layoutPriority(90)
                 }.padding()
--- Sopa de Letrinhas/MainView.swift
+++ Sopa de Letrinhas/MainView.swift
@@ -11,36 +11,71 @@ struct MainView: View {
     
     @AppStorage("onboarding") var didOnboardingHappend = false
     @State var shouldShow = false
+    @State var birthDate = Date.now
     
     var body: some View {
         GeometryReader{ geo in
-            HStack{
-                ZStack{
-                    Rectangle()
-                    BoardView()
+            ZStack {
+                
+                Rectangle().foregroundColor(Color(white: 0.4))
+                
+                HStack{
+                    ZStack{
+                        Rectangle().foregroundColor(.clear)
+                    }
+                    .frame(width: geo.size.width/1.3)
+                    VStack{
+                        ZStack{
+                            Rectangle().foregroundColor(Color(white: 0.3))
+                        }.padding(.horizontal,10)
+                            .frame(height: geo.size.height/4.15)
+                            .padding(.bottom,10)
+                        ZStack{
+                            Rectangle().foregroundColor(Color(white: 0.3))
+                        }.padding(10)
+                        ZStack{
+                            Rectangle().foregroundColor(Color(white: 0.3))
+                        }.padding(.horizontal,10)
+                            .padding(.top,10)
+                            .padding(.bottom,-2)
+                    }
+                }.padding(.top,12)
+                    .padding(.leading, 12)
+                    .padding(.trailing, 5)
+                    .padding(.bottom,18)
+                
+                HStack{
+                    ZStack{
+                        Rectangle().foregroundColor(.white)
+                        BoardView()
+                    }
+                    .frame(width: geo.size.width/1.3)
+                    VStack{
+                        ZStack{
+                            Rectangle().foregroundColor(.white)
+                            StatusView(status: "tô triste")
+                        }.padding(.horizontal,10)
+                            .padding(.bottom,10)
+                            .frame(height: geo.size.height/4)
+                        ZStack{
+                            Rectangle().foregroundColor(.white)
+                            CalendarioView()
+                        }.padding(10)
+                        ZStack{
+                            Rectangle().foregroundColor(.white)
+                            LembretesView()
+                        }.padding(.horizontal,10)
+                            .padding(.top,10)
+                    }
+                }.padding(10)
+                    .padding(.bottom,10)
+                    .sheet(isPresented: $shouldShow, content: {OnboardingView()})
+                
+            }.onAppear(){
+                if !didOnboardingHappend {
+                    shouldShow.toggle()
                 }
-                .frame(width: geo.size.width/1.3)
-                VStack{
-                    ZStack{
-                        Rectangle()
-                        StatusView(status: "tô triste")
-                    }.padding(10)
-                        .frame(height: geo.size.height/4)
-                    ZStack{
-                        Rectangle()
-                        CalendarioView()
-                    }.padding(10)
-                    ZStack{
-                        Rectangle()
-                        LembretesView()
-                    }.padding(10)
-                }
-            }.padding()
-                .sheet(isPresented: $shouldShow, content: {OnboardingView()})
-        }.onAppear(){
-            if !didOnboardingHappend {
-                shouldShow.toggle()
             }
-        }
+        }.frame(minWidth: 900,minHeight: 500)
     }
 }
