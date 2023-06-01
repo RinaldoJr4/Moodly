9e469cb803b5f6f275ffc06da96f8d1407dfcf46
ggs <ggs@MacBook-Pro-8.local>
2023-06-01 08:44:56 -0300

    informacoes nos postit concluido

--- Sopa de Letrinhas/BoardView.swift
+++ Sopa de Letrinhas/BoardView.swift
@@ -4,24 +4,28 @@
 //
 //  Created by rsbj on 25/05/23.
 //
-
 import SwiftUI
 
 struct BoardView: View {
-    
     var currentDate = Date()
     var currentDateCalendar = Calendar.current
+    let today  = Date.now
     var dateFormater: DateFormatter {
         let formater = DateFormatter()
         formater.dateStyle = .medium
         return formater
     }
+    var formatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH"
+        return formatter
+    }
+    @AppStorage("status") var currentStatus = "Triste"
     
     var body: some View {
         GeometryReader { geo in
             ScrollView {
                 HStack {
-                    
                     VStack {
                         HStack {
                             Text("Bom dia, vamos planejar suas atividades?")
@@ -33,7 +37,6 @@ struct BoardView: View {
                         }.padding(geo.size.width/80)
                         
                         TodoView(currentDate: currentDate)
-                        
                         
                     }.padding(geo.size.width/60)
                         .frame(width: geo.size.width/1.4)
@@ -56,45 +59,51 @@ struct BoardView: View {
                                     .minimumScaleFactor(0.8)
                             }.padding(.trailing, geo.size.width/30)
                         }
-                        
                         Spacer().layoutPriority(200)
                         Spacer().layoutPriority(200).frame(height: geo.size.height/6)
+                        
                         ZStack{
-                            VStack{
-                                HStack{
-                                    // Sombra Postit Azul
-                                    Rectangle().foregroundColor(Color(white: 0.7))
-                                        .frame(width: geo.size.width/5, height: geo.size.width/5)
-                                    Spacer()
-                                }.padding(.top,geo.size.width/23)
-                                    .padding(.leading,geo.size.width/400)
-                                HStack{
-                                    Spacer()
-                                    Rectangle().foregroundColor(.clear)
-                                        .frame(width: geo.size.width/5, height: geo.size.width/5)
-                                }
-                            }
-                            VStack{
-                                HStack{
-                                    // Postit Azul
-                                    ZStack{
-                                        Rectangle().foregroundColor(.blue)
+                            if ((Int(formatter.string(from: today)) ?? 0) >= 18){
+                                VStack{
+                                    HStack{
+                                        // Sombra Postit Azul
+                                        Rectangle().foregroundColor(Color(white: 0.7))
                                             .frame(width: geo.size.width/5, height: geo.size.width/5)
-                                        Text("Postit azul")
+                                        Spacer()
+                                    }.padding(.top,geo.size.width/23)
+                                        .padding(.leading,geo.size.width/400)
+                                    HStack{
+                                        Spacer()
+                                        Rectangle().foregroundColor(.clear)
+                                            .frame(width: geo.size.width/5, height: geo.size.width/5)
                                     }
-                                    Spacer()
-                                }.padding(.top,geo.size.width/25)
-                                HStack{
-                                    Spacer()
-                                    Rectangle().foregroundColor(.clear)
-                                        .frame(width: geo.size.width/5, height: geo.size.width/5)
+                                }
+                                
+                                VStack{
+                                    HStack{
+                                        // Postit Azul
+                                        ZStack{
+                                            Rectangle().foregroundColor(.blue)
+                                                .frame(width: geo.size.width/5, height: geo.size.width/5)
+                                            Text("Lembre-se de ao final do dia refletir sobre suas emoções, avaliando se houve mudanças e o que pode ter impactado.")
+                                                .frame(width: 100, height: 100)
+                                        }
+                                        Spacer()
+                                    }.padding(.top,geo.size.width/25)
+                                    HStack{
+                                        Spacer()
+                                        Rectangle().foregroundColor(.clear)
+                                            .frame(width: geo.size.width/5, height: geo.size.width/5)
+                                    }
                                 }
                             }
+                            
                             VStack{
                                 HStack{
                                     Spacer()
                                         .frame(width: geo.size.width/5, height: geo.size.width/5)
                                 }
+                                
                                 HStack{
                                     // Sombra Postit Vermelho
                                     Spacer()
@@ -113,7 +122,8 @@ struct BoardView: View {
                                     ZStack {
                                         Rectangle().foregroundColor(.red)
                                             .frame(width: geo.size.width/5, height: geo.size.width/5)
-                                        Text("Reflex vermelho")
+                                        Text(getTextForStatus(currentStatus))
+                                            .frame(width: 100, height: 100)
                                     }
                                 }.padding(.bottom,geo.size.width/25)
                                     .padding(.trailing,geo.size.width/300)
@@ -123,6 +133,23 @@ struct BoardView: View {
                         .padding(.trailing)
                 }.clipped()
             }.scrollIndicators(.hidden)
+        }
+    }
+    
+    func getTextForStatus(_ status: String) -> String {
+        switch status {
+        case "Triste":
+            return "Tá tudo certo, nem sempre estaremos bem! Talvez você não seja tão produtivo hoje, mantenha isso em mente e busque não se cobrar tanto. Vai dar bom!"
+        case "Ansioso":
+            return "Tá tudo certo, nem sempre estaremos bem! Talvez você não seja tão produtivo hoje, mantenha isso em mente e busque não se cobrar tanto. Vai dar bom!"
+        case "Feliz":
+            return "Que massa! Que tal tentar realizar atividades que você está querendo muito concluir? Lembre de ter cuidado para não se sobrecarregar. Bora simbora!"
+        case "Irritado":
+            return "Sei que não é o melhor momento. Se tiver que fazer algo, que tal priorizar apenas o que é necessário e, se surgir a oportunidade, tirar um tempo para conversar com alguém."
+        case "Neutro":
+            return "Massa! Bora começar o dia com o pé direito organizando as tarefas."
+        default:
+            return ""
         }
     }
 }
